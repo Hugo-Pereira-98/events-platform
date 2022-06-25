a56dc1cf5c48145103fa38d3136f48d7a51c25a8
Hugo Pereira <hugop868@gmail.com>
2022-06-25 16:14:46 -0300

    add lesson html and css

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,20 @@
-export function Lesson(){
-    return <h1>Lesson</h1>
+import { CheckCircle } from 'phosphor-react';
+export function Lesson() {
+	return (
+		<a href="#">
+			<span className="text-gray-300">Terca - 22 junho - 19hrs</span>
+			<div className="rounded border border-gray-500 p-4 mt-2">
+				<header className="flex items-center justify-between">
+					<span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+						<CheckCircle size={20} />
+						Conteudo Liberado
+					</span>
+					<span className="text-xs rounded px-2 py-[2px] text-white border border-green-300 font-bold">
+						Ao vivo
+					</span>
+				</header>
+				<strong className="text-gray-200 mt-5 block">Aberutra da primeira aula</strong>
+			</div>
+		</a>
+	);
 }
