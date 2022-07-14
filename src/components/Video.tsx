48e3b5e1a5e1456551675f3ab842ce8054b3df26
Hugo Pereira <hugop868@gmail.com>
2022-07-14 18:03:12 -0300

    asdd

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,116 +1,129 @@
-import '@vime/core/themes/default.css';
-import { DefaultUi, Player, Youtube } from '@vime/react';
-import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react';
-import { useGetLessonBySlugQuery } from '../graphql/generated';
+import "@vime/core/themes/default.css";
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from "phosphor-react";
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface Props {
-	lessonSlug: string;
+  lessonSlug: string;
 }
 
 export default function Video({ lessonSlug }: Props) {
-	const { data } = useGetLessonBySlugQuery({
-		variables: {
-			slug: lessonSlug,
-		},
-	});
+  const { data } = useGetLessonBySlugQuery({
+    variables: {
+      slug: lessonSlug,
+    },
+  });
 
-	if (!data || !data.lesson) {
-		return (
-			<div className="flex-1">
-				<p>Carregando...</p>
-			</div>
-		);
-	}
+  if (!data || !data.lesson) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    );
+  }
 
-	return (
-		<div className="flex-1">
-			<div className="bg-black flex justify-center">
-				<div className="w-full h-full max-w-[1100px] max-h-[60vh] aspect-video">
-					<Player>
-						<Youtube videoId={data.lesson.videoId} />
-						<DefaultUi />
-					</Player>
-				</div>
-			</div>
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="w-full h-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
+      </div>
 
-			<div className="p-8 max-w-[1100px] mx-auto">
-				<div className="flex items-start gap-16">
-					<div className="flex-1">
-						<h1 className="text-2xl font-bold">{data.lesson.title}</h1>
-						<p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              {data.lesson.description}
+            </p>
 
-						{data.lesson.teacher && (
-							<div className="flex items-center gap-4 mt-6">
-								<img
-									className="w-16 h-16 rounded-full border-2 border-blue-500"
-									src={data.lesson.teacher.avatarURL}
-									alt={data.lesson.teacher.name}
-								/>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="w-16 h-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt={data.lesson.teacher.name}
+                />
 
-								<div className="leading-relaxed">
-									<strong className="text-2xl block">{data.lesson.teacher.name}</strong>
-									<span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
-								</div>
-							</div>
-						)}
-					</div>
+                <div className="leading-relaxed">
+                  <strong className="text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
 
-					<div className="flex flex-col gap-4">
-						<a
-							href="#"
-							className="p-4 text-sm bg-violet-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-violet-700 transition-colors"
-						>
-							<DiscordLogo size="24" />
-							Comunidade do Discord
-						</a>
-						<a
-							href="#"
-							className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-blue-500 hover:text-gray-900 transition-colors"
-						>
-							<Lightning size="24" />
-							Acesse o desafio
-						</a>
-					</div>
-				</div>
+          <div className="flex flex-col gap-4">
+            <a
+              href="#"
+              className="p-4 text-sm bg-violet-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-violet-700 transition-colors"
+            >
+              <DiscordLogo size="24" />
+              Comunidade do Discord
+            </a>
+            <a
+              href="#"
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size="24" />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
 
-				<div className="gap-8 mt-20 grid grid-cols-2">
-					<a
-						href="#"
-						className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
-					>
-						<div className="bg-green-700 h-full p-6 flex items-center">
-							<FileArrowDown size="40" />
-						</div>
-						<div className="py-6 leading-relaxed">
-							<strong className="text-2xl">Material complementar</strong>
-							<p className="text-sm text-gray-200 mt-2">
-								Acesse o material complementar para acelerar o seu desenvolvimento.
-							</p>
-						</div>
-						<div className="h-full p-6 flex items-center">
-							<CaretRight size="24" />
-						</div>
-					</a>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href="#"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size="40" />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Em breve será adicionado seção de aulas complementares para
+                todos tocarmos como o Almirzão.
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size="24" />
+            </div>
+          </a>
 
-					<a
-						href="#"
-						className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
-					>
-						<div className="bg-green-700 h-full p-6 flex items-center">
-							<FileArrowDown size="40" />
-						</div>
-						<div className="py-6 leading-relaxed">
-							<strong className="text-2xl">Wallpapers exclusivos</strong>
-							<p className="text-sm text-gray-200 mt-2">
-								Baixe os wallpapers exclusivos do Ignite Lab e personalize sua máquina.
-							</p>
-						</div>
-						<div className="h-full p-6 flex items-center">
-							<CaretRight size="24" />
-						</div>
-					</a>
-				</div>
-			</div>
-		</div>
-	);
+          <a
+            href="#"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size="40" />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                A adicionar fotos exclusivas da participação do Almir na novela
+                Pantanal de 1990 e também do remake de 2022.
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size="24" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,83 +1,91 @@
-import { FormEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Logo from '../components/Logo';
-import { useCreateSubscriberMutation, useGetLessonSlugQuery } from '../graphql/generated';
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Logo from "../components/Logo";
+import {
+  useCreateSubscriberMutation,
+  useGetLessonSlugQuery,
+} from "../graphql/generated";
 
 export default function Subscribe() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-	const { data } = useGetLessonSlugQuery();
+  const { data } = useGetLessonSlugQuery();
 
-	const [createSubscriber, { loading }] = useCreateSubscriberMutation();
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
-	async function handleSubscribe(e: FormEvent) {
-		e.preventDefault();
+  async function handleSubscribe(e: FormEvent) {
+    e.preventDefault();
 
-		if (!data) return;
+    if (!data) return;
 
-		await createSubscriber({
-			variables: {
-				name,
-				email,
-			},
-		});
+    await createSubscriber({
+      variables: {
+        name,
+        email,
+      },
+    });
 
-		const [firstLesson] = data.lessons;
+    const [firstLesson] = data.lessons;
 
-		navigate(`/event/lesson/${firstLesson.slug}`);
-	}
+    navigate(`/event/lesson/${firstLesson.slug}`);
+  }
 
-	return (
-		<div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-			<div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-				<div className="max-w-[640px]">
-					<Logo />
+  return (
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center px-4">
+      <div className="w-full max-w-[1100px] flex flex-col items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]">
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Esta é uma{" "}
+            <strong className="text-blue-500">playlist completa</strong>, com as
+            melhores músicas da atualidade no{" "}
+            <strong className="text-blue-500">Brasil</strong>
+          </h1>
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            Em apenas uma semana acompanhando as aulas você vai aprender o que é
+            música de verdade, ter assunto com homens de bem e, quem sabe,
+            talvez ainda creça um cabelinho no seu peito.
+          </p>
+        </div>
 
-					<h1 className="mt-8 text-[2.5rem] leading-tight">
-						Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com{' '}
-						<strong className="text-blue-500">React</strong>
-					</h1>
-					<p className="mt-4 text-gray-200 leading-relaxed">
-						Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e
-						com alta demanda para acessar as melhores oportunidades do mercado.
-					</p>
-				</div>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">
+            Inscreva-se gratuitamente
+          </strong>
 
-				<div className="p-8 bg-gray-700 border border-gray-500 rounded">
-					<strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+          <form
+            onSubmit={handleSubscribe}
+            className="flex flex-col gap-2 w-full"
+          >
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="bg-gray-900 rounded px-5 h-14"
+              placeholder="Seu nome completo"
+            />
 
-					<form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-						<input
-							type="text"
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-							className="bg-gray-900 rounded px-5 h-14"
-							placeholder="Seu nome completo"
-						/>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-900 rounded px-5 h-14"
+              placeholder="Digite seu e-mail"
+            />
 
-						<input
-							type="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							className="bg-gray-900 rounded px-5 h-14"
-							placeholder="Digite seu e-mail"
-						/>
-
-						<button
-							type="submit"
-							disabled={loading}
-							className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
-						>
-							Garantir minha vaga
-						</button>
-					</form>
-				</div>
-			</div>
-
-			<img src="/src/assets/code-mockup.png" className="mt-10" alt="Código de programação" />
-		</div>
-	);
+            <button
+              type="submit"
+              disabled={loading}
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+              onClick={() => navigate("/event")}
+            >
+              Garantir minha vaga
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
